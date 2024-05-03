8f3428679b4c1d8b30fc1c1b9df6e891fd0f14a0
Leifeng2000 <leifeng7112@gmail.com>
2024-05-03 15:22:34 +0700

    up

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("nhap diem toan: ");
+        double mathScore;
+        double.TryParse(Console.ReadLine(), out mathScore);
+
+        Console.Write("nhap diem ly: ");
+        double physicsScore;
+        double.TryParse(Console.ReadLine(), out physicsScore);
+
+        Console.Write("nhap diem hoa: ");
+        double chemistryScore;
+        double.TryParse(Console.ReadLine(), out chemistryScore);
+
+        double averageScore = (mathScore + physicsScore + chemistryScore) / 3;
+
+        Console.WriteLine($"diem trung binh: {averageScore}");
+    }
+}
